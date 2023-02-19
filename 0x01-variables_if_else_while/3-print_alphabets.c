979e4356c33d3746ce47438529489405f5aacd1f
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-19 10:39:11 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Enter point
+ * Return: o(Success)
+ */
+int main(void)
+{
+int x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
