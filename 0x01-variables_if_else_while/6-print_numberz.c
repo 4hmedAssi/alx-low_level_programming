5eda214509b2947d84eb8ad88107d8f7f66b7f78
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-19 12:06:56 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Enter point
+ * Return: o(Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(48 + x);
+}
+putchar('\n');
+return (0);
+}
