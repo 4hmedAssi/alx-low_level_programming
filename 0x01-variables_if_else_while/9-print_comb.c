057bf3c2b790c59f74c825bbbc712cf5fbd22c4b
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-19 13:01:10 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for successgi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,15 @@
 int main(void)
 {
 int y;
-for (y = 0; y <= 8; y++)
+for (y = 0; y <= 9; y++)
+{
+if(y<=8)
 {
 putchar(48 + y);
 putchar(',');
+}
 putchar(' ');
 }
-putchar (48 + 9);
+putchar (' ');
 return (0);
 }
