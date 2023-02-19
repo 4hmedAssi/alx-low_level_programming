21e30879b855fb4c3c0406b4806e5f662e06e10b
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-19 13:43:40 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Enter point
+ * Return: o(Success)
+ */
+int main(void)
+{
+int y;
+int x;
+for (y = 0; y <= 8; y++)
+{
+for (x = 1; x <= 9; x++)
+{
+putchar((y % 10) + '0');
+putchar((x % 10) + '0');
+if (y == 8 && x == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar ('\n');
+return (0);
+}
