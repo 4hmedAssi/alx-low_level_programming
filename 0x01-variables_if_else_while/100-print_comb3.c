be3ca408412324be5da002de040b5aab10c7d421
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-19 14:03:25 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ for (y = 0; y <= 8; y++)
 for (x = 1; x <= 9; x++)
 {
 putchar((y % 10) + '0');
-putchar((x % 10) + '0');
+putchar((x++ % 10) + '0');
 if (y == 8 && x == 9)
 continue;
 putchar(',');
