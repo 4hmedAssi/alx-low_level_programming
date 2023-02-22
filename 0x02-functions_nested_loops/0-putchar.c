39014d68ca2521421b0e1fb8be4f352cf39e84df
Ahmed <Ahmed.Assi4849@outlook.com>
2023-02-22 03:12:25 -0800

    0._putchar:wq

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Enter point
+ * Return: 0(success)
+ */
+int main(void)
+{
+for (char *a = "_putchar" ; *a ; a++)
+{
+_putchar(*a);
+}
+_putchar('\n');
+return (0);
+}
