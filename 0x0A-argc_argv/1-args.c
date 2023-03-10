f63d91dd03f37c6ed8c562cf4ab7cb5bfb1e1837
Ahmed <Ahmed.Assi4849@outlook.com>
2023-03-10 06:11:48 -0800

    das

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a program that prints its name, with a new line.
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers
+ * Return: Always (0)
+ */
+int main(int argc, char **argv)
+{
+printf("%d/n", argc);
+return (0);
+}
